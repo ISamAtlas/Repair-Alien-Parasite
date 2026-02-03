6f4c4cde0e620fcc9562c6a6236421f3622c90c3
ISamAtlas <srchytfa@gmail.com>
2026-02-03 17:38:11 -0500

    procedural animation for a single leg

--- scripts/characters/procedural_legs.gd
+++ scripts/characters/procedural_legs.gd
@@ -1,8 +1,13 @@
-extends Marker2D
+extends Node2D
 
-@onready var target: Marker2D = $"../../target"
+@export var target: Marker2D
+@export var distance_threshold:int = 20
 
 func _physics_process(delta: float) -> void:
-	
-	var distance:float = (global_position.x - target.global_position.x)
-	print(distance)
+	var direction := Input.get_axis("left", "right")
+	target.global_position.x += direction * 100 * delta
+	print(abs(global_position.x - target.global_position.x))
+	if abs(global_position.x - target.global_position.x) > distance_threshold:
+		print("yep!")
+		#target.global_position.x = lerp(target.global_position.x, global_position.x, delta)
+		target.global_position.x = global_position.x
