7606971e229e914be86f5d946e65f17465f05cda
ISamAtlas <srchytfa@gmail.com>
2026-02-03 19:22:59 -0500

    procedural legs consistent y level adjustment

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
-@onready var left_leg_target: Marker2D = $"legs/CharacterContainer/IK targets/LeftLeg Target"
+@onready var legs: Node2D = $legs
 
 
 const SPEED = 300.0
@@ -9,12 +9,15 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor():
+		legs.process_mode = Node.PROCESS_MODE_DISABLED
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		legs.process_mode = Node.PROCESS_MODE_INHERIT
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
--- scripts/characters/procedural_legs.gd
+++ scripts/characters/procedural_legs.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
 @export var target: Marker2D
-@export var distance_threshold:int = 20
+@export var distance_threshold:int = 23
 
 func _physics_process(delta: float) -> void:
+	target.global_position.y = global_position.y
 	var direction := Input.get_axis("left", "right")
-	target.global_position.x += direction * 100 * delta
-	print(abs(global_position.x - target.global_position.x))
+	target.global_position.x += direction * -100 * delta
 	if abs(global_position.x - target.global_position.x) > distance_threshold:
-		print("yep!")
-		#target.global_position.x = lerp(target.global_position.x, global_position.x, delta)
 		target.global_position.x = global_position.x
